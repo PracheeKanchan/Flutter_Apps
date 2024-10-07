a7faf9a8843902e3fb575bece08531de16a41cd0
Prachee Kanchan <pracheekanchan@gmail.com>
2024-10-07 12:33:55 +0530

    Assignment1

--- Assignments/assign1_appbar_container/lib/statement_7.dart
+++ Assignments/assign1_appbar_container/lib/statement_7.dart
@@ -20,12 +20,56 @@ class Statement7 extends StatelessWidget{
                 scrollDirection: Axis.horizontal,
                 child: Row(
                     children: [
-                        SizedBox(
-                         // padding: const EdgeInsets.all(20),
-                            height: 300,
-                            width: 150,
-                            //decoration: BoxDecoration(border: Border.all()),
-                            child: Image.network("https://static.toiimg.com/thumb/width-600,height-400,msid-47986046.cms"),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: SizedBox(
+                              height: 300,
+                              width: 150,
+                              child: Image.network("https://i.pinimg.com/236x/b1/5f/e6/b15fe68d3086f6e35c1b996c1d811b74.jpg",
+                                  fit:BoxFit.cover,
+                              ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: SizedBox(
+                              
+                              height: 300,
+                              width: 150,
+                              child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJKqA8PueJMsi4gQRgsyfww7ZnXcN-DdbHkA&s",
+                                  fit:BoxFit.cover,
+                              ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: SizedBox(
+                              height: 300,
+                              width: 150,
+                              child: Image.network("https://i.pinimg.com/originals/f6/35/fd/f635fda7eac6e9315006ecfba15db2b6.jpg",
+                                  fit:BoxFit.cover,
+                              ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: SizedBox(
+                              height: 300,
+                              width: 150,
+                              child: Image.network("https://m.media-amazon.com/images/I/61S+YHHA6xL._AC_UF1000,1000_QL80_.jpg",
+                                  fit:BoxFit.cover,
+                              ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: SizedBox(
+                              height: 300,
+                              width: 150,
+                              child: Image.network("https://i.pinimg.com/236x/b1/5f/e6/b15fe68d3086f6e35c1b996c1d811b74.jpg",
+                                  fit:BoxFit.cover,
+                              ),
+                          ),
                         ),
                         
                     ],
